2443542ffe3831f2a5ff3f301083d13a970a5519
Maxens Dubois <maxens.dubois@gmail.com>
2016-03-15 16:37:10 +0100

    Objets correction

---
+++ Correction/CollectionObjet.js
@@ -0,0 +1,47 @@
+﻿//créer quelques objets
+var objet = {
+		taille: 25,
+		poids: 2,
+		matiere: "Plastique"
+	};
+var objet2 = {
+		taille: 16,
+		poids: 3,
+		matiere: "Metal"
+	};
+var objet3 = {
+		taille: 24,
+		poids: 7,
+		matiere: "Bois"
+	};
+//créer un inventaire
+var inventaire = [objet,objet3,objet3,objet2];
+var compteur = 0;
+function VoirInventaire(){
+	
+	//1 Avant la boucle
+	var result = "L'inventaire contient : "
+	compteur=0;
+	while(true)
+	//2 Dans la boucle
+	{
+	var element = inventaire[compteur];
+		console.log(element);
+		result=result +" "+element.nom;
+		compteur=compteur+1;
+		console.log(compteur);
+
+		
+		if (compteur==inventaire.length)
+			break;
+	
+		//stop et sortie de boucle
+		//Break;
+	
+		//stop et nouveau tour de boucle
+		continue;
+	};
+	
+	//3 Après la boucle
+	return result;
+}
--- Correction/main.js
+++ Correction/main.js
@@ -1,13 +1,1 @@
-﻿function pile(){
-	console.log("vous pensez que se sera pile ?");
-	var x = Math.random() ;
-	if (x>=0.5) {
-		console.log("gagné !!!");
-	}
-	if (x==0.5) {
-		console.log("... joue au loto maintenant");
-	}
-	if (x<=0.5) {
-		console.log("perdu ...");
-	}
-}
+﻿
