9d3a1912f7434cf0ab1d65f526c54b381474e3a3
Herondil <maxens.dubois@gmail.com>
2016-03-25 14:11:19 +0100

    réorganisation des dossiers/fichiers

--- Correction/main.js
+++ Correction/main.js
@@ -15,22 +15,36 @@
 	//création de la balise
 	window.canvas 	= document.createElement("canvas"),
 	window.context = canvas.getContext("2d");
+	canvas.width = 5000;
+	canvas.height = 5000;
 	//ajout de la balise au body
 	document.body.appendChild(canvas);
 	//requestAnimFrame
-	setInterval(GameLoop,1000/60);
+	setInterval(GameLoop,1000/2);
 }
 function GameLoop(){
 	DrawMap();
 }
 
 function DrawMap(){
-	var img = context.createImageData(1,1);
+	var img = context.createImageData(2,1);	
 	//ne pas répéter "img.data[]" !
-	img.data[0] = 130;
-	img.data[1] = 50;
-	img.data[2] = 23;
-	img.data[3] = 255;	
-	context.putImageData(img,10,10);
-	context.putImageData(img,10,15);
+	//premier pixel
+	img.data[0] = Math.round(Math.random()*5);
+	img.data[1] = Math.round(Math.random()*100);
+	img.data[2] = Math.round(Math.random()*25);
+	img.data[3] = 255;
+	//second
+	img.data[4] = Math.round(Math.random()*5);
+	img.data[5] = Math.round(Math.random()*5);
+	img.data[6] = Math.round(Math.random()*255);
+	img.data[7] = 255;
+	
+	for(var j = Math.round(Math.random()*canvas.height); j < canvas.height;
+	j += Math.round(Math.random()*canvas.height) ) {
+		for(var i = Math.round(Math.random()*canvas.width);
+		i < canvas.width; i+=Math.round(Math.random()*canvas.width)){
+			context.putImageData(img,i,j);
+		}	
+	}
 }
